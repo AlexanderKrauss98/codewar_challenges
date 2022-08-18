0c37194cc1a9cdef46801725be9c1cc49f3937fc
Alexander Krauß <111492127+AlexanderKrauss98@users.noreply.github.com>
2022-08-18 14:48:43 +0100

    Create 5kyu_Numbers with The Highest Amount of Divisors.py
    Task:
    An array of different positive integers is given. We should create a code that gives us the number (or the numbers) that has (or have) the highest number of divisors among other data.
    
    The function proc_arrInt(), (Javascript: procArrInt()) will receive an array of unsorted integers and should output a list with the following results:
    
    [(1), (2), (3)]
    
    (1) - Total amount of numbers received
    (2) - Total prime numbers received
    (3) - a list [(a), (b)]
          (a)- The highest amount of divisors that a certain number (or numbers) of the given  
               array have
          (b)- A sorted list with the numbers that have the largest amount of divisors. The list may  
               have only one number
    Examples
    arr1 = [66, 36, 49, 40, 73, 12, 77, 78, 76, 8, 50,
           20, 85, 22, 24, 68, 26, 59, 92, 93, 30]
    
    proc_arrInt(arr1) ------> [21, 2, [9, [36]]
    
    # 21 integers in the array
    # 2 primes: 73 and 97
    # 36 is the number that has the highest amount of divisors:
    # 1, 2, 3, 4, 6, 9, 12, 18, 36 (9 divisors, including 1 and 36)

---
+++ 5kyu_Numbers with The Highest Amount of Divisors.py
@@ -0,0 +1,41 @@
+#NOTE: This code pass all Test but the Runtime is limited on the Side on 12000MS. Normaly its complete if no timelimit would exist. I i would enjoy for Tipps.
+
+#-----------------------------------------------------------------------My Solution-----------------------------------------------------------
+def proc_arrInt(listNum):
+
+    Task_1 = len(listNum) #Task 1 
+    
+    temp_Task_2 = [] #Task 2
+    for k in listNum:                         
+        if k > 1:                           # ---------------------------------------------
+            for i in range(2, int(k/2)+1):  # ---------------------------------------------
+                 if (k % i) == 0:           # ------------clasic prime algorithm-----------
+                    break                   # ---------------------------------------------
+            else:                           # ---------------------------------------------
+                temp_Task_2.append(k)       # ---- if its prime. put it into temp list-----
+                
+    Task_2 = len(temp_Task_2) #count elements of primes 
+      
+    a = 0 #count of bigest divisor
+    b = [] #num with bigest count of divisor 
+    for i in listNum: 
+        temp_count = 0      
+        for d in range (1,i+1): # for 1 - i (range doesnt work exactly for i, so i+1)
+            if i%d == 0: 
+                temp_count += 1 # temp_count +1 if its divisible               
+        if temp_count == a: # add to b if it has the same count of divisor
+            b.append(i) 
+        elif temp_count > a: # set new leader if temp_count is bigger then the actually a 
+            a = temp_count  
+            b = [i]
+    b.sort()
+    
+    return [Task_1, Task_2, [a, b]]
+  
+#---------------------------------------------------------------Testcase Taskgiver------------------------------------------------------------------
+  
+test.describe("Example Tests")
+arr1 = [66, 36, 49, 40, 73, 12, 77, 78, 76, 8, 50, 20, 85, 22, 24, 68, 26, 59, 92, 93, 30] 
+test.assert_equals(proc_arrInt(arr1), [21, 2, [9, [36]]])
+arr2 = [267, 273, 271, 145, 275, 150, 487, 169, 428, 50, 314, 444, 445, 67, 458, 211, 58, 95, 357, 486, 359, 491, 108, 493, 247, 379] 
+test.assert_equals(proc_arrInt(arr2),[26, 7, [12, [108, 150, 444, 486]]])
