c83a769999169f5b6faf0940128b796270da99b0
AlexanderKrauss98 <111492127+AlexanderKrauss98@users.noreply.github.com>
2022-08-18 11:03:55 +0100

    Create  6kyu_Bit Counting.py
    Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
    
    Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case

---
+++ 6kyu_Bit Counting.py
@@ -0,0 +1,21 @@
+#---------------------------------------My Solution-------------------------------
+
+def count_bits(n):   
+    temp = format(n, "b")   #create "b"incode     
+    x = [int(a) for a in str(temp)]   #bring bin code into a list    
+    return (x.count(1))   #counting 1 from list
+  
+#-------------------------------------Testfile from Taskgiver----------------------
+
+import codewars_test as test
+from solution import count_bits
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it("Basic Tests")
+    def basic_tests():
+        test.assert_equals(count_bits(0), 0)
+        test.assert_equals(count_bits(4), 1)
+        test.assert_equals(count_bits(7), 3)
+        test.assert_equals(count_bits(9), 2)
+        test.assert_equals(count_bits(10), 2)
