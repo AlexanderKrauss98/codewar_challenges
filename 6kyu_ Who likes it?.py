f39912c837f1e921cf4ce305398a3c9c6a13fcbe
AlexanderKrauss98 <111492127+AlexanderKrauss98@users.noreply.github.com>
2022-08-18 10:59:45 +0100

    Create 6kyu_ Who likes it?.py
    Task:
    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
    
    Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
    
    []                                -->  "no one likes this"
    ["Peter"]                         -->  "Peter likes this"
    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
    Note: For 4 or more names, the number in "and 2 others" simply increases.

---
+++ 6kyu_ Who likes it?.py
@@ -0,0 +1,28 @@
+#----------------------------------------------------My Solution-----------------------------
+
+def likes(names):
+    count = len(names)
+    if count == 0:
+        return "no one likes this"
+    elif count == 1:
+        return f"{names[0]} likes this"
+    elif count == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif count == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    elif count >= 4:
+        other = count - 2
+        return f"{names[0]}, {names[1]} and {other} others like this"
+ 
+#-----------------------------------------------Testfile from Taskgiver---------------------
+ 
+import codewars_test as test
+from solution import likes
+
+@test.it('Basic tests')
+def _():
+    test.assert_equals(likes([]), 'no one likes this')
+    test.assert_equals(likes(['Peter']), 'Peter likes this')
+    test.assert_equals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this')
+    test.assert_equals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this')
+    test.assert_equals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this')
